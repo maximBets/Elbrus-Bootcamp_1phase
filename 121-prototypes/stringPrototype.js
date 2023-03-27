4b97750c51ff285fe37b0a3b6fb2de3a532d139c
abakharev <abakharev@sharevault.com>
2023-03-27 12:33:14 +0300

    121

--- 121-prototypes/stringPrototype.js
+++ 121-prototypes/stringPrototype.js
@@ -1,6 +1,7 @@
-// eslint-disable-next-line no-extend-native
 // в стрелочной функции нет своего контекста
 // String.prototype.splitWords = () => this.split(' ');
+
+// eslint-disable-next-line no-extend-native
 String.prototype.splitWords = function () {
   this.split(' ');
 };
